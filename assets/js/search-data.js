bdbc759f80e649983130689d072fda7e65ec2060
zzz0326 <51013030+zzz0326@users.noreply.github.com>
2025-07-16 16:35:58 +0000

    Deploying to gh-pages from @ zzz0326/zzz0326.github.io@a6417ae2b438969c0693827bcf9862ac17fe2cdc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,28 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
